c90dc1e01e8156dcb022e0ac5c85f9f9e174e3d6
rperera <ryan.p207@gmail.com>
2019-11-20 18:09:29 +0100

    updated task list functions

--- script.js
+++ script.js
@@ -27,37 +27,84 @@ updateTime();
 
 // Add Task To List 
 
-var tasks = [];  
+// Add task to List - append Child method 
 
-function displayTask() {
-	var task = document.getElementById("newTask").value;
-	 if (task == "" || task == 0)
-	 	{ return false; }
-	 tasks.push(task);
-	document.getElementById("taskList").children[0].innerHTML += "<li>"+tasks[tasks.length-1]+"</li>";
-	task.value = ""
-}	
+var addButton = document.getElementById("add"); 
+var task = document.getElementById("inputTask");
+var taskList = document.getElementById("taskList").children[0]; 
 
-function displayTask2() {
-	var task = document.getElementById("newTask").value;
-	 if (task == "" || task == 0)
-	 	{ return false; }
-	 else if (event.keyCode === 13){ 
-	 tasks.push(task);
-	document.getElementById("taskList").children[0].innerHTML += "<li>"+tasks[tasks.length-1]+"</li>";
-	task.value = ""
+function createListElement() { 
+		var li = document.createElement("li");
+		// need to add here classes to Li and event listeners for strikethrough 
+		li.classList.add("listItem");
+		li.appendChild(document.createTextNode(task.value)); 
+		taskList.appendChild(li);
+		task.value = ""; 
+
+		var delbtn = document.createElement("button");
+		// need to add classes for delbtn and to make it delete parent 
+		delbtn.classList.add("delBtn")
+		delbtn.appendChild(document.createTextNode("X"));
+		li.appendChild(delbtn); 
+		delbtn.addEventListener("click",deleteListItem); 
+
+		function deleteListItem() { 
+		li.classList.add("delete"); 
+		 }
+ }
+
+
+
+function appendTaskClick() {
+	if (task.value.length > 0)  {
+	 createListElement(); 
 	}
-	else {return false;}
+}
+
+function appendTaskEnter(event) {
+	 if (task.value.length > 0 && event.keyCode === 13) { 
+	 createListElement(); 
+	} 
 }	
 
-document.getElementById("add").addEventListener("click",displayTask); 
-document.getElementById("newTask").addEventListener("keypress",displayTask2); 
+addButton.addEventListener("click", appendTaskClick); // Event listener for click on add button 
+task.addEventListener("keypress", appendTaskEnter); //Event lsitenfer for enter on input 
 
-// Store completed tasks
+// innerHTML method 
 
-var completedTasks = []; 
+// var tasks = [];  
 
-// maybe need to use a array.map to move from tasks array to completed 
+// function displayTaskClick() {
+// 	var task = document.getElementById("newTask").value;
+// 	 if (task == "" || task == 0)
+// 	 	{ return false; }
+// 	tasks.push(task);
+// 	document.getElementById("taskList").children[0].innerHTML +="<li>"+"<input type='checkbox' class='tickbox'>"+tasks[tasks.length-1]+"<button class='delete'>X</button>"+"</li>";
+// }	
+
+// function displayTaskEnter() {
+// 	var task = document.getElementById("newTask").value;
+// 	 if (task == "" || task == 0)
+// 	 	{ return false; }
+// 	 else if (event.keyCode === 13){ 
+// 	 tasks.push(task);
+// 	document.getElementById("taskList").children[0].innerHTML +="<li>"+"<input type='checkbox' class='tickbox'>"+tasks[tasks.length-1]+"<button class='delete'>X</button>"+"</li>";
+// 	}
+// 	else {return false;}
+// }	
+
+// addButton.addEventListener("click", displayTaskClick); // Event listener for click on add button 
+// inputEnter.addEventListener("keypress", displayTaskEnter); //Event lsitenfer for enter on input 
+
+// Function to move completed tasks from outstanding to completed 
+
+function moveTask() {
+	var completedTask = document.getElementById("checkbox").value; 
+	if (completedTask = true) {
+		completedTasks.push(completedTask); 
+		document.getElementById("completedTaskList").children[0].innerHTML +="<li>"+"<input type='checkbox' class='checkbox'>"+completedTasks[completedTasks.length-1]+"</li>";
+		}
+}
 
 // NotesList Test
 var testArray = ["This is a note about important things", "Here is another note.", "and a third note!"]

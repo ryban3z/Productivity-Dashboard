915b569bf956fb9f686728cf78d531df6eb72337
rperera <ryan.p207@gmail.com>
2020-01-22 15:16:08 +0100

    updated topics styling and JS - need to complete

--- App/topics.js
+++ App/topics.js
@@ -1,7 +1,5 @@
 var topics = (localStorage.getItem('topicsData')) ? JSON.parse(localStorage.getItem("topicsData")) : [];
 
-const btnClass = ["btn-outline-primary", "btn-outline-secondary", "btn-outline-success", "btn-outline-danger", "btn-outline-dark", "btn-outline-warning", "btn-outline-info"]
-
 const addTopicInput = document.getElementById("addTopic");
 const topicList = document.getElementById("topicList");
 const clearTopic = document.getElementById("clearTopic"); 
@@ -9,7 +7,7 @@ const clearTopic = document.getElementById("clearTopic");
 function addTopic(e) {
     var topic = addTopicInput.value;
     if (topic && e.code === 'Enter') {
-        createTopic(topic); 
+        createTopic2(topic); 
         topics.push(topic); 
         topicsArrayStored(); 
         addTopicInput.value = '';   
@@ -19,10 +17,17 @@ function addTopic(e) {
 function createTopic (topic) {
     let newTopic = document.createElement("button");
         newTopic.classList.add("btn", "d-block", "my-1", "btn-block", "btn-outline-info");
-        // let randomClass = btnClass[Math.floor(Math.random()*btnClass.length)]; 
-        // newTopic.classList.add(randomClass); 
         newTopic.innerHTML = topic;
         topicList.appendChild(newTopic);
+}
+
+function createTopic2 (topic) {
+    let topicBody = document.createElement("div");
+    topicBody.classList.add("container", "border", "topicItem")
+    let topicContent = document.createElement("p"); 
+    topicContent.innerHTML = topic; 
+    topicBody.appendChild(topicContent)
+    topicList.appendChild(topicBody); 
 }
 
 const topicsArrayStored = () => {
@@ -32,17 +37,17 @@ const topicsArrayStored = () => {
 function renderTopics() {
     if (!topics) return;
     for (i=0; i < topics.length; i++) {
-        createTopic(topics[i]); 
+        createTopic2(topics[i]); 
     }
 }; 
-
-renderTopics(); 
 
 function clearTopics() {
     localStorage.removeItem("topicsData"); 
     topics = []; 
     topicList.innerHTML = ''; 
 }; 
+
+renderTopics(); 
 
 addTopicInput.addEventListener("keypress", addTopic);
 clearTopic.addEventListener("click", clearTopics); 
